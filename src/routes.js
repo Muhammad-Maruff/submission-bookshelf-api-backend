14ffced7bcec6b029b0e5e5e8899f3de1202501a
Maruf <muhammadmaruf2001@gmail.com>
2022-04-14 13:42:33 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const {
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, putBookByIdHandler, deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: putBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+module.exports = routes;
